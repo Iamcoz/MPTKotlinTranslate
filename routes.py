eb7971327a2ed9ff1fb5db9aa4b2edabe3df8b1e
coz <cozisdev@gmail.com>
2024-02-06 20:24:40 +0900

    로깅 데이터 상세화

--- routes.py
+++ routes.py
@@ -52,7 +52,16 @@ def add_basic_data():
 # 기초체력 상위 퍼센테이지 산출 및 컬럼 update
 @app.route('/api/BasicData/<string:nickname>/update_rating', methods=['PUT'])
 def update_basic_rating(nickname):
+    # data = request.get_json()
     data = request.json
+    if not data or 'dynamic_column' not in data:
+        return jsonify({"error": "Missing required data"}), 400
+
+    dynamic_column = data['dynamic_column']
+    # 해당 컬럼이 BasicData 모델에 존재하는지 확인
+    if not hasattr(BasicData, dynamic_column):
+        return jsonify({"error": "Invalid dynamic_column"}), 400
+    
     logging.debug(f"Received PUT request data: {data}")  # 요청 받은 데이터 로그
     dynamic_column = data.get("dynamic_column")
     basic_data = BasicData.query.filter_by(znickname=nickname).order_by(BasicData.row_number.desc()).first_or_404()
